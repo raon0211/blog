b901320fd63a0ad563645c1aec9d4d0ecd17aba5
Sojin Park <raon0211@outlook.com>
2018-12-22 17:49:22 +0900

    fix: Fix typeof bug in build

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './containers/App';
 
-if (typeof document !== undefined) {
+if (typeof document !== 'undefined') {
   const render = module.hot ? ReactDOM.render : ReactDOM.hydrate;
   render(<App />, document.getElementById('root'));
 }
