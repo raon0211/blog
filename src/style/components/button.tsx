6d1e1df35256941c7ace152e15255eb5d69c27e4
Sojin Park <raon0211@outlook.com>
2018-12-31 23:32:31 +0900

    feat: Add Button component

---
+++ src/style/components/button.tsx
@@ -0,0 +1,70 @@
+import css from '@emotion/css';
+import * as React from 'react';
+import { Paddings, Margins, Typography, Sizes, Colors } from '../constants';
+import chroma from 'chroma-js';
+
+const baseButton = [
+  Paddings.vertical.mediumSmall,
+  Paddings.horizontal.regular,
+  Margins.right.regular,
+  Typography.textSans,
+  css`
+    font-size: ${Sizes.regular};
+    text-decoration: none;
+  `,
+  css`
+    display: inline-block;
+    border: none;
+    border-radius: 4px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.01), 0 2px 4px 0 rgba(0, 0, 0, 0.02),
+      0 4px 10px 0 rgba(0, 0, 0, 0.015);
+  `,
+  css`
+    cursor: pointer;
+  `,
+  css`
+    transition: background-color 0.2s ease, transform 0.1s ease;
+    transform: translateZ(0);
+    outline: none;
+
+    &:hover {
+      transform: scale(1.01);
+    }
+
+    &:active {
+      transform: scale(0.99);
+    }
+  `,
+];
+
+function ButtonBackground(color: string, { textColor = '#fff' } = {}) {
+  return css`
+    background-color: ${color};
+    color: ${textColor};
+
+    &:hover {
+      background-color: ${chroma(color)
+        .darken(0.3)
+        .hex()};
+    }
+  `;
+}
+
+export const ButtonStyles = {
+  primary: [baseButton, ButtonBackground(Colors.accentMedium)],
+  secondary: [
+    baseButton,
+    ButtonBackground('#e9e9ee', {
+      textColor: Colors.text,
+    }),
+  ],
+};
+
+interface ButtonProps {
+  type?: 'primary' | 'secondary';
+  children: React.ReactNode;
+}
+
+export default function Button({ type = 'primary', children }: ButtonProps) {
+  return <button css={ButtonStyles[type]}>{children}</button>;
+}
