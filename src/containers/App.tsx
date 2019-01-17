75c48177e8becbdf04d5884c1f70c20b52f52bdc
Sojin Park <raon0211@outlook.com>
2019-01-17 21:05:45 +0900

    feat: Change code block style to light theme

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -3,6 +3,7 @@ import { Root, Routes } from 'react-static';
 import '../static/reset.css';
 import '../static/katex.min.css';
 import '../static/style.css';
+import '../static/syntax-github.css';
 import Header from '../components/Header';
 import { Global } from '@emotion/core';
 import { Margins, Typography, Containers } from '../style/constants';
