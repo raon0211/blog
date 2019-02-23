092a2571189f97c449d09f880dce08b4f63ba48e
Sojin Park <raon0211@outlook.com>
2019-02-23 19:51:00 +0900

    chore: Update config and gitignore

--- static.config.js
+++ static.config.js
@@ -28,7 +28,7 @@ function Document({ Html, Head, Body, children, renderMeta }) {
   return (
     <Html lang="ko">
       <Head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta content="IE=edge,chrome=1" httpEquiv="X-UA-Compatible" />
         <link
